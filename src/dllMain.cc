41091b1c6f5ffb732a9312a182c3f3eab0d8ef51
hugy <504650082@qq.com>
2023-03-22 16:57:58 +0800

    指定http端口

--- src/dllMain.cc
+++ src/dllMain.cc
@@ -6,7 +6,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call,
                       LPVOID lpReserved) {
   switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH: {
-      http_start(19088);
+      // http_start(19088);
       break;
     }
     case DLL_THREAD_ATTACH: {
