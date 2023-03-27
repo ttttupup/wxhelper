a097ae334a60869d020475cc017bfd56831a26f0
hugy <504650082@qq.com>
2023-03-27 11:40:57 +0800

    sqlite3 偏移

--- src/get_db_handle.cc
+++ src/get_db_handle.cc
@@ -5,7 +5,7 @@
 #include "new_sqlite3.h"
 #include "pch.h"
 #include "wechat_data.h"
-#define CONTACT_G_PINSTANCE_OFFSET 0x2e2d628
+#define CONTACT_G_PINSTANCE_OFFSET 0x2ffddc8
 #define DB_MICRO_MSG_OFFSET 0x68
 #define DB_CHAT_MSG_OFFSET 0x1C0
 #define DB_MISC_OFFSET 0x3D8
@@ -15,10 +15,10 @@
 #define DB_FUNCTION_MSG_OFFSET 0x11B0
 #define DB_NAME_OFFSET 0x14
 
-#define PUBLIC_MSG_MGR_OFFSET  0x2e6ce20
-#define MULTI_DB_MSG_MGR_OFFSET  0x2e6ec84
-#define FAVORITE_STORAGE_MGR_OFFSET  0x2e6e630
-#define FTS_FAVORITE_MGR_OFFSET  0x2e2e168
+#define PUBLIC_MSG_MGR_OFFSET  0x303df74
+#define MULTI_DB_MSG_MGR_OFFSET  0x30403b8
+#define FAVORITE_STORAGE_MGR_OFFSET  0x303fd40
+#define FTS_FAVORITE_MGR_OFFSET  0x2ffe908
 
 using namespace std;
 map<wstring, DatabaseInfo> dbmap;
@@ -227,7 +227,7 @@ std::vector<void *> GetDbHandles() {
   }
 
   // Favorite.db
-  DWORD favItems_ptr = *(DWORD *)(*(DWORD *)(favorite_storage_mgr_addr) + 0x8);
+  DWORD favItems_ptr = *(DWORD *)(*(DWORD *)(*(DWORD *)(favorite_storage_mgr_addr) + 0x8) + 0x4);
   if (favItems_ptr) {
     DWORD favorite_db_addr = *(DWORD *)(favItems_ptr + 0x38);
     DatabaseInfo favorite_db{0};
