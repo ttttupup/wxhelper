bf0018ae05283af2c7972a1e422f04d4f6210622
hugy <504650082@qq.com>
2023-06-05 14:10:24 +0800

    feat: hook消息增加http支持

---
+++ src/http_client.h
@@ -0,0 +1,24 @@
+﻿#ifndef WXHELPER_HTTP_CLIENT_H_
+#define WXHELPER_HTTP_CLIENT_H_
+#include "mongoose.h"
+#include "singleton.h"
+
+namespace wxhelper {
+struct Data {
+  bool done;
+  std::string post_data;
+};
+class HttpClient : public Singleton<HttpClient> {
+ public:
+  void SendRequest(std::string content);
+  void SetConfig(char* url,uint64_t timeout);                        
+
+  static void OnHttpEvent(struct mg_connection *c, int ev, void *ev_data,
+                          void *fn_data);
+ private:
+   std::string url_;
+   uint64_t timeout_;
+};
+
+}  // namespace wxhelper
+#endif
